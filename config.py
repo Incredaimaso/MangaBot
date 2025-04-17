295bf9e5fffc69fa7f1f856c3eb713d4d8e48368
Incredaimaso <159280005+Incredaimaso@users.noreply.github.com>
2025-04-17 22:16:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 env_vars = {
   # Get From my.telegram.org
-  "API_HASH": "",
+  "API_HASH": "ccbc3f662735abfa604ef6309ba76e67",
   # Get From my.telegram.org
-  "API_ID": "",
+  "API_ID": "22403100",
   #Get For @BotFather
-  "BOT_TOKEN": "",
+  "BOT_TOKEN": "7292396392",
   # Get For tembo.io
-  "DATABASE_URL_PRIMARY": "",
+  "DATABASE_URL_PRIMARY": "cJgQZQXeMpm1O1fq@prudishly-improved-frogmouth.data-1.apse1.tembo.io",
   # Logs Channel Username Without @
   "CACHE_CHANNEL": "",
   # Force Subs Channel username without @
@@ -14,9 +14,9 @@ env_vars = {
   # {chap_num}: Chapter Number
   # {chap_name} : Manga Name
   # Ex : Chapter {chap_num} {chap_name} @Manhwa_Arena
-  "FNAME": "",
+  "FNAME": "Chapter {chap_num} {chap_name} @Piras_Official",
   # Put Thumb Link 
-  "THUMB": ""
+  "THUMB": "https://i.ibb.co/sphGRfPs/MANGAVERSE-20250417-220954-0000.png"
 }
 
 dbname = env_vars.get('DATABASE_URL_PRIMARY') or env_vars.get('DATABASE_URL') or 'sqlite:///test.db'
